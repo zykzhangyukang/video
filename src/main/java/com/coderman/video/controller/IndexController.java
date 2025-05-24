a18f37dc038737319b9cd47d7759eb11b4e85f10
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2025-05-24 12:20:12 +0800

    异常页面布局完成

--- src/main/java/com/coderman/video/controller/IndexController.java
+++ src/main/java/com/coderman/video/controller/IndexController.java
@@ -1,16 +1,18 @@
 package com.coderman.video.controller;
 
 import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * @author ：zhangyukang
  * @date ：2025/05/20 17:54
  */
+@Controller
 public class IndexController {
 
     @ApiOperation(value = "首页路由", notes = "首页路由")
-    @GetMapping(value = {"/index", "/"})
+    @GetMapping(value = {"/"})
     public String indexPage() {
         return "index";
     }
